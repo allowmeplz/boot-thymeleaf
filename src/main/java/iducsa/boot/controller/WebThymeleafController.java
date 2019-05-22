6fb0ea6b4cb7af9dc6b6ba3c7b760711602e91b6
jihun <wlgnsdl356@gmail.com>
2019-05-22 17:41:09 +0900

    commit 190522

--- src/main/java/iducsa/boot/controller/WebThymeleafController.java
+++ src/main/java/iducsa/boot/controller/WebThymeleafController.java
@@ -1,13 +1,18 @@
 package iducsa.boot.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -27,7 +32,7 @@ public class WebThymeleafController {
 		return "index";
 	}
 	
-	@GetMapping("/register") 
+	@GetMapping("/user-reg-form") 
 	public String getRegForm(Model model) {
 		return "form";
 	}
@@ -48,12 +53,40 @@ public class WebThymeleafController {
 	@GetMapping("/users/{id}")
 	public String getUserById(@PathVariable(value = "id") Long userId, Model model)
 			throws ResourceNotFoundException {
-		User user = userRepo.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + userId));
+		User user = userRepo.findById(userId).get(); //.orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + userId));
 		// Java Lambda
-		
+		/*model.addAttribute("id", user.getId());
 		model.addAttribute("name", user.getName());
-		model.addAttribute("company", user.getCompany());
+		model.addAttribute("company", user.getCompany());*/
+		model.addAttribute("user",user);
 		return "user";
 		//return ResponseEntity.ok().body(user);
 	}
+	
+	@GetMapping("/users/fn")
+	public String getUserByName(@Param(value="name") String name, Model model)
+			throws ResourceNotFoundException {
+		List<User> users = userRepo.findByName(name);
+		model.addAttribute("users",users);
+		return "userlist";
+		//return ResponseEntity.ok().body(user);
+	}
+	
+	@PutMapping("/users/{id}")
+	public String updateUser(@PathVariable(value = "id") Long userId,@Valid User userDetails, Model model) {
+		User user = userRepo.findById(userId).get();
+		user.setName(userDetails.getName());
+		user.setCompany(userDetails.getCompany());
+		userRepo.save(user);
+		model.addAttribute("users", userRepo.findAll());
+		return "redirect:/users";
+	}	
+	
+	@DeleteMapping("/users/{id}")
+	public String deleteUser(@PathVariable(value = "id") Long userId, Model model) {
+		User user = userRepo.findById(userId).get();
+		userRepo.delete(user);
+		model.addAttribute("name", user.getName());
+		return "user-deleted";
+	}	
 }
--- src/main/java/iducsa/boot/repositories/UserRepository.java
+++ src/main/java/iducsa/boot/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package iducsa.boot.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import iducsa.boot.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+	public List<User> findByName(String name);
 }
